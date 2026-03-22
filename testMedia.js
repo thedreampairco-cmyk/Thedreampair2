92cdd3895d6cd83355a7bd5bc50160d8ddc3ce68
Ankit Verma <thedreampair.co@gmail.com>
2026-03-22 14:57:23 +0530

    Auto-update: 2026-03-22 14:57:23

--- testMedia.js
+++ testMedia.js
@@ -1,7 +1,7 @@
 import { sendImageMessage } from "./services/whatsapp/greenApiMedia.js";
 
 await sendImageMessage(
-  "91XXXXXXXXXX@c.us",
+  "918426862111@c.us",
   "https://your-direct-image-url",
   "Test product image"
 );
